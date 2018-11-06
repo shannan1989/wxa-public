b9162aa19397f842a9f978909fa367647b271868
shannan <zhangpeichaook@163.com>
2018-11-06 22:09:16 +0800

    #pages/index/index

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,45 @@
+import client from '../../libs/client/client.js';
 import WxPage from '../../libs/client/wxPage.js';
 
 WxPage({
     wxPage: {
-        enableFormSubmit: true
+        enableFormSubmit: true,
+        enablePageRequest: true
     },
-    data: {},
-    onLoad: function () { }
+    data: {
+        loadStatus: 'loading'
+    },
+    onLoad: function (options) {
+        this.loadData(false);
+    },
+    onRefresh: function () {
+        this.loadData(false);
+    },
+    loadData: function (isUpdate = null) {
+        this.pageRequest({
+            isUpdate: isUpdate,
+            auth: {
+                type: 'full',
+                success: (res) => {
+                    console.log('auth success', res);
+                },
+                fail: (res) => {
+                    console.log('auth fail', res);
+                }
+            },
+            request: {
+                type: 'List',
+                data: {},
+                success: (res) => {
+                    console.log('request success', res);
+                },
+                fail: (res) => {
+                    console.log('request fail', res);
+                }
+            }
+        });
+    },
+    onTapButton: function () {
+
+    }
 });
