2e52f95991362012490ae52a3a5944956f42789b
shannan <zhangpeichaook@163.com>
2018-11-06 22:17:59 +0800

    #

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 let settings = {
-    debug: true,
-    requestUrl: '',
+    debug: false,
+    requestUrl: 'https://www.shannanshuibei.net/service/demo/a.php',
     debugRequestUrl: 'http://localhost/service/demo/a.php',
     app: 'demo',
     appName: '微信小程序',
